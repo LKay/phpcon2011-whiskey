d55cea2f819042f0c0977d6946e30d29874b5dd0
Karol Janyst <karol@marucode.com>
2011-10-23 11:04:12 +0200

    Fix logs dir

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -88,7 +88,7 @@ Kohana::init(array(
 /**
  * Attach the file write to logging. Multiple writers are supported.
  */
-Kohana::$log->attach(new Log_File(APPPATH.'logs'));
+Kohana::$log->attach(new Log_File(sys_get_temp_dir()));
 
 /**
  * Attach a file reader to config. Multiple readers are supported.
